4f851ef4990b3da83e2588cf7cd7db6676a52b2e
clintt-09 <aisosaclinton09@gmail.com>
2024-12-22 20:03:45 +0100

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { Analytics } from "@vercel/analytics/react"
 import localFont from 'next/font/local'
 import './globals.css'
 
@@ -43,6 +44,7 @@ export default function RootLayout({
                 className={`text-center ${montserratRegular.variable}  ${montserratExtraBold.variable} ${montserratBold.variable} antialiased`}
             >
                 {children}
+                <Analytics />
             </body>
         </html>
     )
